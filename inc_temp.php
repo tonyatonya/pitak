80227dc2609adb56de805968255819e83b9c09fd
Athapong Athamasirigun <tonya_0@hotmail.com>
2015-09-19 11:38:01 +0700

    Building Update 2

---
+++ inc_temp.php
@@ -0,0 +1,280 @@
+<div class="status-child" data-panel-name="temp">
+	<div class="graph-row">
+		<div class="graph-child tempgraph">
+			<div class="pivot-name">Medical Air</div>
+			<div class="graph-body hasscale smallscale temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>200</span></div>
+						<div class="scale-grid"><span>150</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>bar</span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump A</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump B</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">160</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump C</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">160</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump D</div>
+			</div>
+		</div>
+		<div class="graph-child tempgraph">
+			<div class="pivot-name">Surgical Air </div>
+			<div class="graph-body hasscale smallscale temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>200</span></div>
+						<div class="scale-grid"><span>150</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>bar</span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump A</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump B</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">160</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump C</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">160</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump D</div>
+			</div>
+		</div>
+		<div class="graph-child tempgraph">
+			<div class="pivot-name">Vacuum</div>
+			<div class="graph-body hasscale smallscale temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>200</span></div>
+						<div class="scale-grid"><span>150</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>bar</span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump A</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump B</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">160</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump C</div>
+			</div>
+		</div>
+		<div class="graph-child tempgraph">
+			<div class="pivot-name">E-Vac</div>
+			<div class="graph-body hasscale smallscale temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>200</span></div>
+						<div class="scale-grid"><span>150</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>50</span></div>
+						<div class="scale-grid"><span>bar</span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump A</div>
+			</div>
+			<div class="graph-body temp">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+					</div>
+					<div class="graph-amount">
+						<div class="tempscale">
+							<div class="height"></div>
+							<div class="normal"></div>
+							<div class="low"></div>
+						</div>
+					</div>
+				</div>
+				<div class="bottom-name">Pump B</div>
+			</div>
+		</div>
+	</div>
+</div>
