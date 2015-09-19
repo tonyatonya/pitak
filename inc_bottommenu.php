53ffc030c5f5ea4e353e3e5fcf1385209c7698dd
Athapong Athamasirigun <tonya_0@hotmail.com>
2015-09-19 11:38:07 +0700

    Building Update 2

---
+++ inc_bottommenu.php
@@ -0,0 +1,43 @@
+<div class="bottombar">
+	<div class="bottom-tab hor-tab">
+		<ul>
+			<li data-panel="pressure">
+				<a href="#">
+					<div class="btntrail"></div>
+					<div class="menutext">
+						PRESSURE STATUS
+						<div class="status"></div>
+					</div>
+				</a>
+			</li>
+			<li data-panel="temp">
+				<a href="#">
+					<div class="btntrail"></div>
+					<div class="menutext">
+						TEMPERATURE STATUS
+						<div class="status"></div>
+					</div>
+				</a>
+			</li>
+			<li data-panel="hour">
+				<a href="#">
+					<div class="btntrail"></div>
+					<div class="menutext">
+						HOUR METER
+						<div class="status"></div>
+					</div>
+				</a>
+			</li>
+			<li data-panel="gasflow">
+				<a href="#">
+					<div class="btntrail"></div>
+					<div class="menutext">
+						GAS FLOW METER
+						<div class="status"></div>
+					</div>
+				</a>
+			</li>
+		</ul>
+	</div>
+	<div class="systemtime">23 AUG 2016    11:01 AM</div>
+</div>
---
+++ inc_pressure.php
@@ -0,0 +1,266 @@
+<div class="status-child" data-panel-name="pressure">
+	<div class="graph-row">
+		<div class="rate">
+			<div class="height">High</div>
+			<div class="normal">Operation<br>Range</div>
+			<div class="low">Low</div>
+		</div>
+		<div class="graph-child" data-gtype="lox">
+			<div class="pivot-name">LOX</div>
+			<div class="graph-body hasscale lox">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>2000</span></div>
+						<div class="scale-grid"><span>1500</span></div>
+						<div class="scale-grid"><span>1200</span></div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>400</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Tank1</div>
+			</div>
+			<div class="graph-body lox">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Tank2</div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="o2">
+			<div class="pivot-name">Oxygen Status</div>
+			<div class="graph-body hasscale o2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>2000</span></div>
+						<div class="scale-grid"><span>1500</span></div>
+						<div class="scale-grid"><span>1200</span></div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>400</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Left Bank</div>
+			</div>
+			<div class="graph-body o2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Right Bank</div>
+			</div>
+			<div class="graph-body hasscale smallscale o2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>100</span></div>
+						<div class="scale-grid"><span>80</span></div>
+						<div class="scale-grid"><span>60</span></div>
+						<div class="scale-grid"><span>40</span></div>
+						<div class="scale-grid"><span>20</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Supply</div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="nos">
+			<div class="pivot-name">Nitrous Oxide</div>
+			<div class="graph-body hasscale abnormal nos">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>2000</span></div>
+						<div class="scale-grid"><span>1500</span></div>
+						<div class="scale-grid"><span>1200</span></div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>400</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Left Bank</div>
+			</div>
+			<div class="graph-body nos">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Right Bank</div>
+			</div>
+			<div class="graph-body hasscale smallscale nos">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>100</span></div>
+						<div class="scale-grid"><span>80</span></div>
+						<div class="scale-grid"><span>60</span></div>
+						<div class="scale-grid"><span>40</span></div>
+						<div class="scale-grid"><span>20</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Supply</div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="co2">
+			<div class="pivot-name">Carbondioxide</div>
+			<div class="graph-body hasscale co2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>2000</span></div>
+						<div class="scale-grid"><span>1500</span></div>
+						<div class="scale-grid"><span>1200</span></div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>400</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Left Bank</div>
+			</div>
+			<div class="graph-body co2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"><span></span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Right Bank</div>
+			</div>
+			<div class="graph-body hasscale smallscale co2">
+				<div class="amount">1400</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>100</span></div>
+						<div class="scale-grid"><span>80</span></div>
+						<div class="scale-grid"><span>60</span></div>
+						<div class="scale-grid"><span>40</span></div>
+						<div class="scale-grid"><span>20</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Supply</div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="air">
+			<div class="pivot-name" style="padding-left: 15px;">Air</div>
+			<div class="graph-body hasscale smallscale med">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>100</span></div>
+						<div class="scale-grid"><span>80</span></div>
+						<div class="scale-grid"><span>60</span></div>
+						<div class="scale-grid"><span>40</span></div>
+						<div class="scale-grid"><span>20</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Med. Air</div>
+			</div>
+			<div class="graph-body hasscale smallscale sur">
+				<div class="amount">100</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">PSI</div>
+						<div class="scale-grid"><span>200</span></div>
+						<div class="scale-grid"><span>160</span></div>
+						<div class="scale-grid"><span>120</span></div>
+						<div class="scale-grid"><span>80</span></div>
+						<div class="scale-grid"><span>40</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name">Surgical Air</div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="vacuum">
+			<div class="pivot-name">Vacuum</div>
+			<div class="graph-body hasscale bigscale vac">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">mmHG</div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>640</span></div>
+						<div class="scale-grid"><span>480</span></div>
+						<div class="scale-grid"><span>320</span></div>
+						<div class="scale-grid"><span>160</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name"></div>
+			</div>
+		</div>
+		<div class="graph-child" data-gtype="evac">
+			<div class="pivot-name">E-Vac</div>
+			<div class="graph-body hasscale bigscale evac">
+				<div class="amount">60</div>
+				<div class="graph-frame">
+					<div class="scale">
+						<div class="unit">mmHG</div>
+						<div class="scale-grid"><span>800</span></div>
+						<div class="scale-grid"><span>640</span></div>
+						<div class="scale-grid"><span>480</span></div>
+						<div class="scale-grid"><span>320</span></div>
+						<div class="scale-grid"><span>160</span></div>
+						<div class="scale-grid"></div>
+					</div>
+					<div class="graph-amount"></div>
+				</div>
+				<div class="bottom-name"></div>
+			</div>
+		</div>
+	</div>
+</div>
